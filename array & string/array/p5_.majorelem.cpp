a56a04e44554a089723a0b22c90099acec1aecde
jatin <jatinsharma2231@gmail.com>
2025-10-27 19:04:52 +0530

    Add implementation of majority element algorithm

---
+++ array & string/array/p5_.majorelem.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public: int majorityElement(vector < int > & arr) {
+        int n = arr.size();
+        int candidate = arr[0];
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = arr[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+};
